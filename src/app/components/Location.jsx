bd1858fa15d5653d5e4b95169015922a342f8fb3
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-10-21 08:43:24 +0545

    changes

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -1,9 +1,9 @@
 export default function Location(){
   return (
-    <div className='flex flex-col justify-center items-center p-10 space-y-6 m-auto bg-white'>
+    <div className='flex flex-col justify-center items-center p-4 space-y-6 m-auto bg-white'>
         <h1 className='text-2xl font-cutom underline transition-transform duration-300 hover:scale-150 font-custom text-center mb-6'>Location and Working Hours</h1>
         <div className='flex flex-col md:flex-row justify-between md:space-x-10 space-y-8 md:w-3/4 items-center justify-center '>
-            <div className='md:p-10 bg-fuchsia-200 rounded-2xl p-4 mt-4 '>
+            <div className='bg-fuchsia-200 rounded-2xl p-4 mt-4 '>
               <div className='p-6 rounded-2xl bg-fuchsia-100 w-64'>
                 <h2 className='text-xl font-custom mt-2'>Location</h2>
                 <p className='text-md font-custom mt-2'>1581 Commonwealth</p>
@@ -12,7 +12,7 @@ export default function Location(){
               </div>
                 
             </div>
-            <div className='md:p-10 bg-fuchsia-200 rounded-2xl p-4 '>
+            <div className=' bg-fuchsia-200 rounded-2xl p-4 '>
               <div className='p-6 rounded-2xl bg-fuchsia-100 w-64'>
                   <h2 className='text-xl font-custom mt-4'> Working Hours</h2>
                   <p className='text-md font-custom mt-2'>Weekdays: 10:00 AM - 7:00 PM</p> 
